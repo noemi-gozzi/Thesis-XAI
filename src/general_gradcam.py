28e2228d43c680d23b0950e0938ef72a618e7ff3
noemi <noemi.gozzi@mail.polimi.it>
2020-07-15 12:21:36 +0200

    update gradcam with function and main

--- src/general_gradcam.py
+++ src/general_gradcam.py
@@ -12,7 +12,7 @@ import pandas as pd
 # Xtrainset, Ytrainset, Xtestset, Ytestset=load_data_CNN()
 # num_patient=len(Xtrainset)
 # #for patient in range(1):
-patient=0
+
 # #patient 0
 #
 # #saved_model.summary()
@@ -27,55 +27,61 @@ patient=0
 #     pickle.dump(Xtest, f)
 # #
 
-#tempraory dataset
-with open('/tmp/Xtest_tmp.pkl', 'rb') as f:
-    Xtest = pickle.load(f)
-##model
-saved_model = load_model(('C:\\Users\\noemi\\Desktop\\university\\university\\tesi\\Thesis-XAI\\resources\\best_models\\best_modelinc{}.h5'.format(patient)), custom_objects={'RReLU': RReLU})
-####preds array
-preds=saved_model.predict(Xtest)
-labels_preds=np.argmax((preds), axis=1)
-print(labels_preds)
-print((labels_preds.shape))
 
-####backprop model
-model_tmp=build_model(saved_model)
-guided_model = build_guided_model(saved_model)
-guided_model.summary()
+def compute_gradcam(patient, Xtest):
 
-###divide dataset per label
-Xtest_list=[]
-for label in range(len(np.unique(labels_preds))):
-    Xtest_list.append(Xtest[labels_preds==label])
+    saved_model = load_model(('../resources/best_models/best_modelinc{}.h5'.format(patient)), custom_objects={'RReLU': RReLU})
+    ####preds array
+    preds=saved_model.predict(Xtest)
+    labels_preds=np.argmax((preds), axis=1)
+    print(labels_preds)
+    print((labels_preds.shape))
 
-# for label in range (np.unique(labels_preds)):
-#     gradcam, gb, guided_gradcam = compute_saliency(saved_model, guided_model, './ciao.png', np.expand_dims(test_1[0,:,:,:],
-#                                                    layer_name='conv2d_140',
-#                                                    cls=-1, visualize=False, save=False)
+    ####backprop model
+    model_tmp=build_model(saved_model)
+    guided_model = build_guided_model(saved_model)
+    guided_model.summary()
 
-label=0
+    ###divide dataset per label
+    Xtest_list=[]
+    for label in range(len(np.unique(labels_preds))):
+        Xtest_list.append(Xtest[labels_preds==label])
 
-gradcam_result={}
-for label in range(len(np.unique(labels_preds))):
-    gradcam_value = np.zeros((50, 10, 512))
-    X_tmp = Xtest_list[label]
-    for index in range(50):
-        gradcam, gb, guided_gradcam = compute_saliency(saved_model, guided_model, './ciao.png',
-                                                   np.expand_dims(X_tmp[index, :, :, :], axis=0),
-                                                        layer_name='conv2d_140',
-                                                        cls=-1, visualize=False, save=False)
-        gradcam_value[index, :, :]=gradcam
-    mean_val = np.mean((gradcam_value), axis=0)
-    plt.figure(figsize=(30, 10))
-    plt.imshow(mean_val, cmap='jet', aspect='auto', alpha=0.5)
-    # plt.imshow(x[0,:,:,0], cmap='Greys')
-    plt.savefig(
-        'C:\\Users\\noemi\\Desktop\\university\\university\\tesi\\Thesis-XAI\\resources\\images\\gradcam_label{}_meanvalue_50.jpg'.format(
-            label))
+    # for label in range (np.unique(labels_preds)):
+    #     gradcam, gb, guided_gradcam = compute_saliency(saved_model, guided_model, './ciao.png', np.expand_dims(test_1[0,:,:,:],
+    #                                                    layer_name='conv2d_140',
+    #                                                    cls=-1, visualize=False, save=False)
 
-    plt.show()
-    gradcam_result[label]={'gradcam_values': gradcam_value, 'mean': mean_val}
+    label=0
 
+    gradcam_result={}
+    for label in range(7,8):
+    # for label in range(len(np.unique(labels_preds))):
+        gradcam_value = np.zeros((50, 10, 512))
+        X_tmp = Xtest_list[label]
+        number_instances=X_tmp.shape[0]
+        for index in range(50):
+            gradcam, gb, guided_gradcam = compute_saliency(saved_model, guided_model, './ciao.png',
+                                                       np.expand_dims(X_tmp[index, :, :, :], axis=0),
+                                                            layer_name='conv2d_140',
+                                                            cls=-1, visualize=False, save=False)
+            gradcam_value[index, :, :]=gradcam
+        mean_val = np.mean((gradcam_value), axis=0)
+        plt.figure(figsize=(30, 10))
+        plt.imshow(mean_val, cmap='jet', aspect='auto', alpha=0.5)
+        # plt.imshow(x[0,:,:,0], cmap='Greys')
+        plt.savefig(
+            '../resources/images/gradcam_label{}_meanvalue_50.jpg'.format(
+                label))
 
+        plt.show()
+        gradcam_result[label]={'gradcam_values': gradcam_value, 'mean': mean_val}
+    return gradcam_result
 
+if __name__ == "__main__":
+    # tempraory dataset
+    with open('/tmp/Xtest_tmp.pkl', 'rb') as f:
+        Xtest = pickle.load(f)
+    ##model
 
+    compute_gradcam(patient=0, Xtest=Xtest)
